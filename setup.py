ce5b66142615812950dee2ab13d4b8051937f610
tetsuya.zama <tetsuya.zama@gmail.com>
2015-12-08 00:25:35 +0900

    setup.pyにflaskへの依存を追加

--- setup.py
+++ setup.py
@@ -7,5 +7,8 @@ setup(
     name = "SmartSpeaker",
     version = "0.1",
     packages = find_packages(),
-    test_suite = 'test'
+    test_suite = 'test',
+    install_requires=[
+          'flask',
+    ]
 )
